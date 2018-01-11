28f981c13a06b07b7d41afd3b475f3aa9cc7b156
Alex Corstorphine <alex@abcwebdesigner.com>
2018-01-10 22:50:05 -0800

    updating ec2_tag_instance

--- actions/ec2_tag_instance.py
+++ actions/ec2_tag_instance.py
@@ -1,2 +1,51 @@
-def test():
-	print("asb")
+import boto3
+import json
+import os
+import re
+from botocore.exceptions import ClientError
+
+### Add Tags to EC2 ###
+#Tag format: AUTO: EC2_tag_instance (key:value)"
+def run_action(message):
+	#House keeping - set up variables	
+	instance = message['Entity']['Id']
+	region = message['Entity']['Region']
+	region = region.replace("_","-")
+	compliance_tags = message['Rule']['ComplianceTags'].split("|")
+
+	#initialize ec2
+	ec2 = boto3.client('ec2', region_name=region)
+
+	for tag in compliance_tags:
+		tag = tag.strip() #Sometimes the tags come through with trailing or leading spaces. 
+		remediate_tag = re.match(r'AUTO:\sec2_tag_instance\s\((?P<key>.+):(?P<value>.+)\)', tag) #we just want the tag that matches "AUTO: ec2_tag_instance (key:value)"
+
+		if remediate_tag:
+			key = remediate_tag.group(1)
+			value = remediate_tag.group(2)
+
+			try:	
+				#Apply the tags
+				tag_instance = ec2.create_tags(
+				    Resources=[instance],
+				    Tags=[
+				        {
+				            'Key': key,
+				            'Value': value
+				        }
+				    ]
+				)
+				
+				responseCode = tag_instance['ResponseMetadata']['HTTPStatusCode']
+
+				if responseCode >= 400:
+					text_output = ("Unexpected error:" + tag_instance + "\n")
+				else:
+					text_output = ("Instance tagged: " + instance + "\nKey: " + key + " | Value: " + value + " \n")
+
+			except (ClientError, AttributeError) as e:
+				text_output = ("Unexpected error: %s" % e + "\n")
+
+	return text_output 
+
+
