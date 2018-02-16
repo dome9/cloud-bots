605f5197ac6094cf8480bdf376f0cee9c86cd386
Alex Corstorphine <alex@dome9.com>
2018-02-16 02:08:47 -0700

    fix post_to_sns issues

--- send_events_and_errors.py
+++ send_events_and_errors.py
@@ -3,7 +3,7 @@ from botocore.exceptions import ClientError
 
 
 #Post the event. Currently this goes to SNS but this can be generalized if needed
-def sendEvent(text_output_array,SNS_TOPIC_ARN,post_to_sns): 
+def sendEvent(text_output_array,SNS_TOPIC_ARN): 
 	text_output_array.append("-------------------------\n")
 	#turn it from an array to a string
 	text_output_str = ''.join(text_output_array)
