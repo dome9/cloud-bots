e62aab560cc1d2fd9a1bf75e01cd901b3942373b
Alex Corstorphine <alex@abcwebdesigner.com>
2018-02-23 18:57:05 -0800

    added s3 bucket creation

--- actions/cloudtrail_enable.py
+++ actions/cloudtrail_enable.py
@@ -3,7 +3,7 @@ import json
 from botocore.exceptions import ClientError
 
 # Usage:
-# AUTO: cloudtrail_enable <trailName> <S3BucketName>
+# AUTO: cloudtrail_enable <trailName> <S3bucket_name>
 
 # Settings: 
 # IsMultiRegionTrail: True (CIS for AWS V 1.1.0 Section 2.1)
@@ -16,21 +16,86 @@ from botocore.exceptions import ClientError
 # s3:PutObject
 # cloudtrail:CreateTrail
 
-def run_action(rule,entity,params): 
+#update bucket policy / create bucket
+
+
+# Create S3 bucket
+def make_bucket(account_id,bucket_name):
+
+    try:
+        s3 = boto3.resource('s3', region_name='us-east-1')
+        result = s3.create_bucket(Bucket=bucket_name)
+        text_output = "Bucket %s will be used for storing trails\n" % bucket_name
+
+    except ClientError as e:
+        text_output = "Unexpected error: %s \n" % e
+
+    return text_output
+
+
+# Add S3 policy
+def add_bucket_policy(account_id,bucket_name):
+    bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AWSCloudTrailAclCheck20150319",
+                "Effect": "Allow",
+                "Principal": {"Service": "cloudtrail.amazonaws.com"},
+                "Action": "s3:GetBucketAcl",
+                "Resource": "arn:aws:s3:::" + bucket_name
+            },
+            {
+                "Sid": "AWSCloudTrailWrite20150319",
+                "Effect": "Allow",
+                "Principal": {"Service": "cloudtrail.amazonaws.com"},
+                "Action": "s3:PutObject",
+                "Resource": "arn:aws:s3:::" + bucket_name + "/AWSLogs/" + account_id + "/*",
+                "Condition": {"StringEquals": {"s3:x-amz-acl": "bucket-owner-full-control"}}
+            }
+        ]
+    }
+
+    try:
+        # Create an S3 client
+        s3 = boto3.client('s3')
+
+        # Convert the policy to a JSON string
+        bucket_policy = json.dumps(bucket_policy)
+
+        # Set the new policy on the given bucket
+        result = s3.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
+        
+        responseCode = result['ResponseMetadata']['HTTPStatusCode']
+        if responseCode >= 400:
+            text_output = "Unexpected error: %s \n" % str(result)
+        else:
+            text_output = "Bucket policy updated for cloudtrail delivery.\n"
+
+    except ClientError as e:
+        text_output = "Unexpected error: %s \n" % e
+
+    return text_output
+
+
+
+
+# Create trail
+def create_trail(params,bucket_name): 
     cloudtrail = boto3.client('cloudtrail')
     # Check params for usable values and if not - go to defaults
     try: # Params[0] should be the traffic type. 
         trailName = params[0]
-        bucketName = params[1].lower()
-        text_output = "CloudTrail name will be %s and data will be sent to the S3 bucket: %s \n" % (trailName,bucketName)
+        text_output = "CloudTrail name will be %s and data will be sent to the S3 bucket: %s \n" % (trailName,bucket_name)
     except:
-        text_output = "BOTH trailName and bucketName need to be specified in the tag. Please set the variables and try again\n"
-        return text_output
-
+        trailName = allRegionTrail
+        text_output = "Trail name not set. Defaulting to allRegionTrail.\n"
+        
     try:
         result = cloudtrail.create_trail(
             Name=trailName, ### REQUIRED
-            S3BucketName=bucketName, ### REQUIRED
+            S3BucketName=bucket_name, ### REQUIRED
+            S3KeyPrefix = '',
             IncludeGlobalServiceEvents=True, 
             IsMultiRegionTrail=True, 
             EnableLogFileValidation=True
@@ -54,11 +119,21 @@ def run_action(rule,entity,params):
                 text_output = text_output +  "Unexpected error: %s \n" % e
 
     except ClientError as e:
-        error = e.response['Error']['Code']
-        if error == 'InvalidS3BucketNameException':
-            #If the policy isn't there - add it into the account
-             text_output =  "S3 bucket name %s is invalid. Please verify and try again.\n" % bucketName
-        else:
-            text_output = "Unexpected error: %s \n" % e
+        text_output = "Unexpected error: %s \n" % e
+
+    return text_output
+
+
+def run_action(rule,entity,params): 
+    account_id = entity['accountNumber']
+    bucket_name = "acct%scloudtraillogs" % account_id
+
+    try:
+        text_output = make_bucket(account_id,bucket_name) 
+        text_output = text_output + add_bucket_policy(account_id,bucket_name) 
+        text_output = text_output + create_trail(params,bucket_name)
+        
+    except ClientError as e:
+        text_output = "Unexpected error: %s \n" % e
 
     return text_output
