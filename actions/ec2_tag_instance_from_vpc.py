96982c6bbdf4da923b99574c57963733f4747dd9
Alex Corstorphine <alex@dome9.com>
2018-04-03 16:01:52 -0700

    updated usage to pull specifics from VPC

--- actions/ec2_tag_instance_from_vpc.py
+++ actions/ec2_tag_instance_from_vpc.py
@@ -1,41 +1,49 @@
 import boto3
 
-### Take tags from VPC and add them as tags to EC2 ### 
-# Tag format: AUTO: ec2_tag_instance_from_vpc
+### If an instance is missing a specific tag, try to pull it from the VPC. 
+# Tag format: AUTO: ec2_tag_instance_from_vpc <Key>
 
 def run_action(rule,entity, params):
     instance = entity['id']
     region = entity['region']
     region = region.replace("_","-")
-    tags = entity['vpc']['tags']
+    vpc_tags = entity['vpc']['tags']
     vpc_id = entity['vpc']['id']
-    
-    text_output = "Tagging instance with tags from VPC (id: %s)\n" % vpc_id
 
-    for tag in tags:
-        key = tag['key']
-        value = tag['value']
+    try:
+        key = params[0]
+    except:
+        text_output = "USAGE: AUTO: ec2_tag_instance_from_vpc <Key>\nNo key was found. Skipping.\n"
+        return text_output
 
-        if not value:
-            text_output = text_output + "Key \"%s\" has an empty value. Skipping\n" % key
-            continue
+    instance_tagged = False
 
-        ec2 = boto3.client('ec2', region_name=region)
-        result = ec2.create_tags(
-            Resources=[instance],
-            Tags=[
-                {
-                    'Key': key,
-                    'Value': value
-                }
-            ]
-        )
+    text_output = "Checking instance VPC tags for %s tag\n" % key
+
+    for tag in vpc_tags:
+        vpc_key = tag['key']
+        vpc_value = tag['value']
+
+        if key == vpc_key:
+            ec2 = boto3.client('ec2', region_name=region)
+            result = ec2.create_tags(
+                Resources=[instance],
+                Tags=[
+                    {
+                        'Key': vpc_key,
+                        'Value': vpc_value
+                    }
+                ]
+            )
         
-        responseCode = result['ResponseMetadata']['HTTPStatusCode']
-        if responseCode >= 400:
-            text_output = text_output + "Unexpected error: %s \n" % str(result)
-        else:
-            text_output = text_output + "Instance tagged: %s \nKey: %s | Value: %s \n" % (instance,key,value)
+            responseCode = result['ResponseMetadata']['HTTPStatusCode']
+            if responseCode >= 400:
+                text_output = text_output + "Unexpected error: %s \n" % str(result)
+            else:
+                text_output = text_output + "Instance tagged: %s \nKey: %s | Value: %s \n" % (instance,vpc_key,vpc_value)
+                instance_tagged = True
+
+    if not instance_tagged:
+        text_output = text_output + "Did not find the %s key in the VPC tags. Please check the VPC and try again.\n" % key
 
     return text_output
-
