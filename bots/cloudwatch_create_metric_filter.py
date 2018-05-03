deee2000ddc2262f4998fc1db9f517871876e6cc
Alex Corstorphine <alex@dome9.com>
2018-05-03 08:08:42 -0500

    added in SNS subscriber and metric alarm

--- bots/cloudwatch_create_metric_filter.py
+++ bots/cloudwatch_create_metric_filter.py
@@ -1,8 +1,9 @@
 '''
 ## cloudwatch_create_metric_filter
 What it does: Creates CloudWatch Metric Filters to match the CIS Benchmark
-Usage: cloudwatch_create_metric_filter <filter1> <filter2> .... 
+Usage: AUTO: cloudwatch_create_metric_filter <email_address> <filter1> <filter2> .... 
 Limitations: Cloudtrail needs to be set up to send the logs to a CloudWatchLogs group first.   
+Default: SNS topic name is CloudTrailMetricFilterAlerts
 '''
 
 import boto3
@@ -10,10 +11,138 @@ import json
 import re
 from botocore.exceptions import ClientError
 
+def create_filter(boto_session,log_group,filter_name,filter_pattern,metric_name,metric_value):
+  cloudwatchlogs_client = boto_session.client('logs')
+
+  try:
+    #Give the config somewhere to go
+    result = cloudwatchlogs_client.put_metric_filter(
+        logGroupName=log_group,
+        filterName=filter_name,
+        filterPattern=filter_pattern,
+        metricTransformations=[
+            {
+                'metricName': metric_name,
+                'metricNamespace': 'CISBenchmark',
+                'metricValue': metric_value
+            },
+        ]
+    )
+
+    responseCode = result['ResponseMetadata']['HTTPStatusCode']
+    if responseCode >= 400:
+        text_output = "Unexpected error: %s \n" % str(result)
+    else:
+        text_output = "Metric filter created for filter: %s \n" % filter_name  
+
+  except ClientError as e:
+      text_output = "Unexpected error: %s \n" % e
+
+  return text_output
+
+def create_topic(boto_session,topic_name):
+    sns_client = boto_session.client('sns')
+
+    try:
+        #Give the config somewhere to go
+        result = sns_client.create_topic(Name=topic_name)
+ 
+        responseCode = result['ResponseMetadata']['HTTPStatusCode']
+        if responseCode >= 400:
+            text_output = "Unexpected error: %s \n" % str(result)
+        else:
+            text_output = "SNS topic created (or already existed): %s \n" % topic_name
+
+    except ClientError as e:
+        text_output = "Unexpected error: %s \n" % e
+
+    return text_output
+
+
+def create_subscription(boto_session,topic_arn,email_address):
+    sns_client = boto_session.client('sns')
+
+    try: # Check if there's already a subscription. If it's already there and we re-add it then they get another confirmation email which can be annoying.
+        result = sns_client.list_subscriptions_by_topic(TopicArn=topic_arn)
+
+        for subscription in result['Subscriptions']:
+            if subscription['Endpoint'] == email_address:
+                text_output = "Email address %s already has a subscription to this topic %s. Skipping\n" % (email_address,topic_arn)
+                return text_output
+
+    except ClientError as e:
+            text_output = "Unexpected error: %s \n" % e
+
+    try:
+        #Give the config somewhere to go
+        result = sns_client.subscribe(
+            TopicArn=topic_arn,
+            Protocol='email',
+            Endpoint=email_address
+        )
+        
+        responseCode = result['ResponseMetadata']['HTTPStatusCode']
+        if responseCode >= 400:
+            text_output = "Unexpected error: %s \n" % str(result)
+        else:
+            text_output = "SNS subscription created for: %s \n" % email_address
+
+    except ClientError as e:
+        text_output = "Unexpected error: %s \n" % e
+
+    return text_output 
+
+
+def create_alarm(boto_session,filter_name,topic_arn):
+    cloudwatch_client = boto_session.client('cloudwatch')
+
+    try:
+      #Give the config somewhere to go
+      alarm_name = filter_name + ' Alarm'
+      result = cloudwatch_client.put_metric_alarm(
+          AlarmName=alarm_name, 
+          AlarmDescription='Metric alarm for CloudTrail Metrics', 
+          ActionsEnabled=True,
+          AlarmActions=[topic_arn],
+          MetricName=filter_name,
+          Namespace='CISBenchmark',
+          Statistic='Sum',
+          Period=300,
+          EvaluationPeriods=1,
+          Threshold=1,
+          ComparisonOperator='GreaterThanOrEqualToThreshold'
+      )
+
+      
+      responseCode = result['ResponseMetadata']['HTTPStatusCode']
+      if responseCode >= 400:
+          text_output = "Unexpected error: %s \n" % str(result)
+      else:
+          text_output = "CloudWatch alarm created: \"%s\" \n" % alarm_name
+
+    except ClientError as e:
+        text_output = "Unexpected error: %s \n" % e
+
+    return text_output 
+
+
+
 def run_action(boto_session,rule,entity,params):
     text_output = ""
+    region = entity['region']
+    region = region.replace("_","-")
+    accountNumber = entity['accountNumber']
 
+    topic_name = "CloudTrailMetricFilterAlerts" + region
+    topic_arn = 'arn:aws:sns:' + region + ':' + accountNumber + ':' + topic_name
     log_group = entity['cloudWatchLogsLogGroupArn']
+
+    #Validate the rule was set up properly
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", params[0]):
+      text_output = "No email found as the first parameter\nUsage: AUTO: cloudwatch_create_metric_filter <email_address> <filter1> <filter2> .... \n"
+      return text_output
+    else:
+      email_address = params[0]
 
     if log_group == None or log_group == "":
         text_output = "Cloudtrail is not set up to send to a CloudWatchLogs group. Please update and try again\n"
@@ -89,12 +218,10 @@ def run_action(boto_session,rule,entity,params):
       }
     }
 
-    cloudwatchlogs_client = boto_session.client('logs')
 
     # Loop through the params
     # For each filter, if matching, update variables and set filter
-    for filter_name in params:
-        
+    for filter_name in params[1:]: 
         # Check to make sure we know the filter that we're trying to add
         try:
             metric_name = filter_name
@@ -104,28 +231,19 @@ def run_action(boto_session,rule,entity,params):
             text_output = text_output + "Metric filter %s not found. Please check spelling and try again.\nAvailable filters are: UnauthorizedApiCalls, NoMfaConsoleLogins, RootAccountLogins, IamPolicyChanges, CloudTrailConfigurationChanges, FailedConsoleLogins, DisabledOrDeletedCmks, S3BucketPolicyChanges, AwsConfigChanges, SecurityGroupChanges, NetworkAccessControlListChanges, NetworkGatewayChanges, RouteTableChanges, VpcChanges \n" % filter_name
             continue
 
+        #Do work
         try:
-            #Give the config somewhere to go
-            result = cloudwatchlogs_client.put_metric_filter(
-                logGroupName=log_group,
-                filterName=filter_name,
-                filterPattern=filter_pattern,
-                metricTransformations=[
-                    {
-                        'metricName': metric_name,
-                        'metricNamespace': 'CISBenchmark',
-                        'metricValue': metric_value
-                    },
-                ]
-            )
-
-            responseCode = result['ResponseMetadata']['HTTPStatusCode']
-            if responseCode >= 400:
-                text_output = text_output + "Unexpected error: %s \n" % str(result)
-            else:
-                text_output = text_output + "Metric filter created for filter: %s \n" % filter_name
-
+          text_output = text_output + create_filter(boto_session,log_group,filter_name,filter_pattern,metric_name,metric_value)
+          text_output = text_output + create_topic(boto_session,topic_name)
+          text_output = text_output + create_alarm(boto_session,filter_name,topic_arn)
         except ClientError as e:
-            text_output = text_output + "Unexpected error: %s \n" % e
+          text_output = text_output + "Unexpected error: %s \n" % e  
+
+    #We only want to run this once so we'll do it at the end
+    try:
+      text_output = text_output + create_subscription(boto_session,topic_arn,email_address)
+    except ClientError as e:
+      text_output = text_output + "Unexpected error: %s \n" % e  
 
     return text_output 
+
