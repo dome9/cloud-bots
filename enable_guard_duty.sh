6ad2f47d10e370a8ea40cbf3e6f0b98ca0b71d93
alpalwal <alex@dome9.com>
2018-08-08 13:07:33 -0700

    formatting

--- enable_guard_duty.sh
+++ enable_guard_duty.sh
@@ -1,7 +1,6 @@
 # enable_guard_duty.sh <lambda_arn> <aws_profile_name>
 
 declare -a regions=("us-east-2" "us-east-1" "us-west-1" "us-west-2" "ap-northeast-1" "ap-northeast-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1")
-#declare -a regions=("us-east-2" "ap-northeast-1")
 
 lambda_arn=$1
 
