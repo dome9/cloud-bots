ae2fb29564224c7f5df8b758234dfdd4e73f657d
Ravid Atias <ravida@checkpoint.com>
2019-06-05 10:58:15 +0300

    CR fixes - Remove redundant data from logs

--- send_logs.py
+++ send_logs.py
@@ -7,10 +7,6 @@ from time import gmtime, strftime
 SUMO_HTTP_ENDPOINT = 'https://endpoint4.collection.us2.sumologic.com/receiver/v1/http/ZaVnC4dhaV3N-lPxodY4J3xadi2by444XVaSboLlcbfMeGhqAnZn4PIVuJw_h3EzhhCv4jEFLfhHO3nbvfVgVSiRrB2X1hedSvXwyKB31hF3zdmR7j7mrQ=='
 
 
-def getTimeStamp():
-    timeStamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    timeStamp += "Z"
-    return timeStamp
 
 
 def send_logs(message, start_time, vendor):
@@ -19,9 +15,11 @@ def send_logs(message, start_time, vendor):
     output_type = os.getenv('OUTPUT_TYPE', '')
     execution_time = time.time() - start_time
     session = requests.Session()
+    for bot in message.get('Rules violations found'):
+        del bot['ID']
+        del bot['Name']
     headers = {"Content-Type": "application/json", "Accept": "application/json", "X-Sumo-Name": message.get('Account id'), "X-Sumo-Category": vendor}
-    data = {'timestamp': getTimeStamp(),
-            'msg': message,
+    data = {'msg': message,
             'account_mode': account_mode,
             'cross_account_role_name': cross_account_role_name,
             'output_type': output_type,
