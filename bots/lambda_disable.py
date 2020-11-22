44a5801bb7b71319292d47b3d8d5c9f655670277
hod-harush <73786051+hod-harush@users.noreply.github.com>
2020-11-22 19:04:57 +0200

    update the lambda_disable bot
    fixed all the requested changes in the PR, that made up after the first upload of the code

--- bots/lambda_disable.py
+++ bots/lambda_disable.py
@@ -1,9 +1,11 @@
 '''
-## disable_lambda
+## lambda_disable
 What it does:
     Disable lambda function (by put function concurrency = 0),
-    called by "Lambda Function Code was Updated by an enitity which assumed a Role" alert.
-Usage: AUTO: disable_lambda
+Sample GSL:
+    Lambda Function Code was Updated by an enitity which assumed a Role
+Usage:
+    AUTO: disable_lambda
 Limitations: none
 '''
 
@@ -12,13 +14,10 @@ from botocore.exceptions import ClientError
 ### Disable lambda function - core method
 def run_action(boto_session, rule, entity, params):
     text_output = ''
-    lambda_function_arn = entity['id']
-    # get the lambda function name from the arn
-    lambda_function_name = lambda_function_arn.split("function:")[1]
-    text_output += "lambda function: %s was changed by entity which assumed a role\n" % lambda_function_name
+    lambda_function_name = entity.get('name')
+    lambda_client = boto_session.client('lambda')
 
     # try to set the function concurrency value to 0, than the function will not be able to run:
-    lambda_client = boto_session.client('lambda')
     try:
         response = lambda_client.put_function_concurrency(
             FunctionName=lambda_function_name,
@@ -26,8 +25,8 @@ def run_action(boto_session, rule, entity, params):
         )
 
         if response['ResponseMetadata']['HTTPStatusCode'] == 200: # concurrency changed was done successfully
-            text_output += "lambda function: %s disabled (function concurrency set to 0)\n" % lambda_function_name
-        else: # error with concurrency change
+            text_output = "lambda function: %s disabled (function concurrency set to 0)\n" % lambda_function_name
+        else: # error with the server (concurrency change not done)
             text_output = "can't disable the lambda function: %s, Unexpected error: %s \n" % (lambda_function_name, response)
 
     except ClientError as e: # error with put_function_concurrency request
