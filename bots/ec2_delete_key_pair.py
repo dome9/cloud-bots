61ad6e79a7fecce0928f7dbf20b0e346dd572daf
liranzu <liranzu@checkpoint.com>
2020-11-24 17:21:11 +0200

    Last changes

--- bots/ec2_delete_key_pair.py
+++ bots/ec2_delete_key_pair.py
@@ -16,15 +16,12 @@ def run_action(boto_session, rule, entity, params):
         #Delete the keypair
         ec2_client = boto_session.client('ec2')
         result = ec2_client.delete_key_pair(KeyName=keypair)
-        text_output = "Keypair %s was deleted successfully\n" % keypair
+        text_output = f"Keypair {keypair} was deleted successfully.\n"
 
-    #Code 400
     except ClientError as e:
-        responseCode = result['ResponseMetadata']['HTTPStatusCode']
-        text_output = "Unexpected error: %s \n" % str(result)
+        text_output = f"Client error: {e}.\n"
 
-    #Code 500 and other errors
     except Exception as e:
-        text_output = "Unexpected error: %s \n" % e
+        text_output = f"Unexpected error: {e}.\n"
 
     return text_output
