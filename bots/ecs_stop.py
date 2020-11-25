0117c5dcb5fff0fafe8de351c9a25f9bdc602320
Alin-Lo <73160734+Alin-Lo@users.noreply.github.com>
2020-11-25 11:38:01 +0200

    Update ecs_stop.py
    except error changed

--- bots/ecs_stop.py
+++ bots/ecs_stop.py
@@ -11,41 +11,51 @@ from botocore.exceptions import ClientError
 
 
 def stop_instance(ecs_client, cluster, instance):
-    #function will stop an instance by changing it's state to 'Draining'
+    
+    # function will stop an instance by changing it's state to 'Draining'
+    
     text_output = ''
     try:
         ecs_client.update_container_instances_state(cluster=cluster, containerInstances=[instance,], status='DRAINING')
         
-        text_output = 'Instance {instance} successfully stopped \n'
+        text_output = f'Instance {instance} successfully stopped \n'
 
     except ClientError as e:
-        text_output = 'Unexpected error: %s , error code: %s\n' % e % e.response['ResponseMetadata']['HTTPStatusCode'] 
+
+        text_output = f'Unexpected error: {e}\n'
+        if 'ResponseMetadata' in e.response and 'HTTPStatusCode' in e.response['ResponseMetadata']:
+            text_output += f"error code: {e.response['ResponseMetadata']['HTTPStatusCode']}\n"
 
     return text_output
 
+
 def stop_task(ecs_client, cluster, task):
-    #function will stop task
+
+    # function will stop task
+
     text_output = ''
     try:
         ecs_client.stop_task(cluster=cluster, task=task, reason='Privileged task is dangerous and unnecessary')
-        
-        text_output = 'Task %s successfully stopped \n' % str(task)
+        text_output = f'Task {task} successfully stopped \n'
 
     except ClientError as e:
-        text_output = 'Unexpected error: %s , error code: %s\n' % e % e.response['ResponseMetadata']['HTTPStatusCode']
+
+        text_output = f'Unexpected error: {e}\n'
+        if 'ResponseMetadata' in e.response and 'HTTPStatusCode' in e.response['ResponseMetadata']:
+            text_output += f"error code: {e.response['ResponseMetadata']['HTTPStatusCode']}\n"
 
     return text_output
 
 
-def run_action(boto_session,rule,entity,params):
+def run_action(boto_session, rule, entity, params):
 
-    id = entity['id']
-    role_arn = id[:id.find(',')]
+    entity_id = entity.get('id')
+    role_arn = entity_id[:entity_id.find(',')]
     ecs_client = boto_session.client('ecs')
     
     text_output = ''
     
-    #check if client has active clusters.
+    # check if client has active clusters.
     clusters = ecs_client.list_clusters()['clusterArns']
     if len(clusters) == 0:
         text_output = '0 clusters exist for user. No tasks running!\n'
@@ -55,34 +65,31 @@ def run_action(boto_session,rule,entity,params):
         
         tasks = ecs_client.list_tasks(cluster=cluster, desiredStatus='RUNNING')['taskArns']
        
-        #check if client has running tasks.
+        # check if client has running tasks.
         if len(tasks) != 0:
             
             for task in tasks:
                 described = ecs_client.describe_tasks(cluster=cluster, tasks=[task,])['tasks'][0]
-                task_defenition = described['taskDefinitionArn']
+                task_definition = described.get('taskDefinitionArn')
                 
-                #check if task defenition of running tasks is secure and if not than task is stopped.
-                defenition = ecs_client.describe_task_definition(taskDefinition=task_defenition)['taskDefinition']
-                
-                #if task runs on an instance, instance needs to be stopped so more tasks like that wont be created.
-                if 'EC2' in defenition['compatibilities']:
-                    text_output = stop_instance(ecs_client, cluster, described['containerInstanceArn'])
-                        
-                    if text_output.find('error') != -1:
-                        return text_output
-                    print(text_output)
-                
-                if defenition['executionRoleArn'] == role_arn:
+                # check if task definition of running tasks is secure and if not than task is stopped.
+                definition = ecs_client.describe_task_definition(taskDefinition=task_definition)['taskDefinition']
+
+                if definition.get('executionRoleArn') == role_arn:
                     
                     text_output = stop_task(ecs_client, cluster, task)
                     if text_output.find('error') != -1:
                         return text_output
     
                     print(text_output)
-                    
+                    # if task runs on an instance, instance needs to be stopped so more tasks like that wont be created.
+                    if 'EC2' in definition.get('compatibilities'):
+                        text_output = stop_instance(ecs_client, cluster, described.get('containerInstanceArn'))
 
-    
+                        if text_output.find('error') != -1:
+                            return text_output
+                    print(text_output)
+
     if text_output == '':
         text_output = 'Running tasks do not exist.\nExiting\n' 
             
