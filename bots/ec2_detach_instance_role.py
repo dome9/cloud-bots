bb5c5ac0e6546c2031907b1e233283acdf00d15b
Alin-Lo <73160734+Alin-Lo@users.noreply.github.com>
2020-11-25 11:09:31 +0200

    Update ec2_detach_instance_role.py
    except error modified

--- bots/ec2_detach_instance_role.py
+++ bots/ec2_detach_instance_role.py
@@ -19,17 +19,20 @@ def run_action(boto_session, rule, entity, params):
     try:
         response = iam.list_instance_profiles_for_role(RoleName=role_name)['InstanceProfiles']
         if len(response) == 0:
-            text_output = f'The {role_name} role is not attached to an instance.\nExiting'
+            text_output = f'The {role_name} role is not attached to instance.\nExiting\n'
             return text_output
 
         iam.remove_role_from_instance_profile(
             InstanceProfileName=response[0]['InstanceProfileName'],
             RoleName=role_name
         )
-        text_output = text_output + 'Role successfully detached from instance\n'
+
+        text_output = f'The {role_name} role successfully detached from instance\nExiting'
 
     except ClientError as e:
-        text_output = 'Unexpected error: %s , error code: %s\n' % e % e.response['ResponseMetadata']['HTTPStatusCode']
-    except Exception as e:
-        text_output = 'Unexpected error: %s\n' % e
+
+        text_output = f'Unexpected error: {e}\n'
+        if 'ResponseMetadata' in e.response and 'HTTPStatusCode' in e.response['ResponseMetadata']:
+            text_output += f"error code: {e.response['ResponseMetadata']['HTTPStatusCode']}\n"
+
     return text_output
