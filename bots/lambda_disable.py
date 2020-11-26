aff57ffac36a961e0b5e851f561c987fd0d18ee0
hod-harush <73786051+hod-harush@users.noreply.github.com>
2020-11-26 18:49:58 +0200

    Update bot lambda_disable for upload

--- bots/lambda_disable.py
+++ bots/lambda_disable.py
@@ -1,9 +1,9 @@
 '''
 ## lambda_disable
 What it does:
-    Disable lambda function (by put function concurrency = 0),
+    Disable lambda function (by put function concurrency = 0).
 Sample GSL:
-    Lambda Function Code was Updated by an enitity which assumed a Role
+    cloudtrail where event.name like 'UpdateFunctionCode%' and issuer.type='Role'
 Usage:
     AUTO: lambda_disable
 Limitations: none
@@ -11,22 +11,22 @@ Limitations: none
 
 from botocore.exceptions import ClientError
 
-### Disable lambda function - core method
+### Disable lambda function
 def run_action(boto_session, rule, entity, params):
     text_output = ''
     lambda_function_name = entity.get('name')
     lambda_client = boto_session.client('lambda')
 
-    # try to set the function concurrency value to 0, than the function will not be able to run:
+    # try to set the function concurrency value to 0
     try:
         response = lambda_client.put_function_concurrency(
             FunctionName=lambda_function_name,
             ReservedConcurrentExecutions=0
         )
 
-        text_output = "lambda function: %s disabled (function concurrency set to 0)\n" % lambda_function_name
+        text_output = f'lambda function: {lambda_function_name} disabled successfully\nExiting'
 
-    except ClientError as e: # error with put_function_concurrency request
-        text_output = "can't disable the lambda function: %s, Unexpected error: %s \n" % (lambda_function_name, e)
+    except ClientError as e:
+        text_output = f"can't disable the lambda function: {lambda_function_name}, Unexpected error: {e} \n"
 
     return text_output
