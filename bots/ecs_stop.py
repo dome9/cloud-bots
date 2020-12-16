080bb9354e3e906bffa80c229b841152b8daa8d1
Alin-Lo <73160734+Alin-Lo@users.noreply.github.com>
2020-12-16 11:35:22 +0200

    Update ecs_stop.py
    getting role arn from entity changed and instance draining is before stop task.

--- bots/ecs_stop.py
+++ bots/ecs_stop.py
@@ -2,7 +2,6 @@
 ## ecs_stop
 What it does: stops an ecs tasks and ec2 instances which contain the tasks
 Usage: AUTO: ecs_stop
-
 Sample GSL: cloudtrail where event.name='RegisterTaskDefinition' and event.status='Success'
 Limitations: none
 '''
@@ -48,8 +47,8 @@ def stop_task(ecs_client, cluster, task):
 
 def run_action(boto_session, rule, entity, params):
 
-    entity_id = entity.get('id')
-    role_arn = entity_id[:entity_id.find(',')]
+    role_arn = entity.get('id')
+    
     ecs_client = boto_session.client('ecs')
     
     text_output = ''
@@ -69,6 +68,7 @@ def run_action(boto_session, rule, entity, params):
             
             for task in tasks:
                 described = ecs_client.describe_tasks(cluster=cluster, tasks=[task,])['tasks'][0]
+                
                 task_definition = described.get('taskDefinitionArn')
                 
                 # check if task definition of running tasks is secure and if not than task is stopped.
@@ -76,17 +76,17 @@ def run_action(boto_session, rule, entity, params):
 
                 if definition.get('executionRoleArn') == role_arn:
                     
-                    text_output = stop_task(ecs_client, cluster, task)
-                    if text_output.find('error') != -1:
-                        return text_output
-    
-                    print(text_output)
                     # if task runs on an instance, instance needs to be stopped so more tasks like that wont be created.
-                    if 'EC2' in definition.get('compatibilities'):
+                    if 'EC2' in described.get('launchType'):
                         text_output = stop_instance(ecs_client, cluster, described.get('containerInstanceArn'))
 
                         if text_output.find('error') != -1:
                             return text_output
+                        
+                    text_output += stop_task(ecs_client, cluster, task)
+                    if text_output.find('error') != -1:
+                        return text_output
+
                     print(text_output)
 
     if text_output == '':
