9cf819f1456925ce0af8b15872bacc51ee6dcccc
Alin-Lo <73160734+Alin-Lo@users.noreply.github.com>
2020-12-23 12:52:38 +0200

    Update vpc_isolate.py
    text out put is global now and ec2 client is used instead ec2 resource

--- bots/vpc_isolate.py
+++ bots/vpc_isolate.py
@@ -12,37 +12,48 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
+text_output = str()
 
-def create_acl(vpc):
 
-    text_output = ''
+def create_acl(ec2_client, vpc_id):
+
     # An array which will contain all vpc subnets
     association_ids = []
 
     try:
         # getting all acls within the vpc, to get the subnet associations from them
-        network_acl_iterator = vpc.network_acls.all()
+        network_acl_iterator = ec2_client.describe_network_acls(
+            Filters=[{
+                    'Name': 'vpc-id',
+                    'Values': [
+                        vpc_id,
+                    ]},
+            ],)
 
         # Going through all each acl and checking for associated subnets
-        for acl in network_acl_iterator:
-            associations = acl.associations
+        for acl in network_acl_iterator.get('NetworkAcls'):
+
+            associations = acl.get('Associations')
             if associations:
                 # gets all subnets from the acl in an array and adding it to association_ids array
                 association_ids += [association.get('NetworkAclAssociationId') for association in associations]
-                
+
         # checking if there any subnets to associate with the new acl
         if association_ids:
 
             # creating new acl with deny all rules
-            network_acl = vpc.create_network_acl()
+            network_acl = ec2_client.create_network_acl(VpcId=vpc_id)
 
             # associating each subnet with the new acl
             for id in association_ids:
-                network_acl.replace_association(
+                ec2_client.replace_network_acl_association(
                     AssociationId=id,
+                    DryRun=False,
+                    NetworkAclId=network_acl.get('NetworkAclId')
                 )
 
     except ClientError as e:
+        global text_output
         text_output = f'Unexpected error: {e}\n'
 
     return text_output
@@ -50,7 +61,7 @@ def create_acl(vpc):
 
 def attach_policy_to_all_users(boto_session, policy_arn):
 
-    text_output = ''
+    global text_output
     iam_client = boto_session.client('iam')
 
     # getting all users in the account
@@ -74,7 +85,6 @@ def attach_policy_to_all_users(boto_session, policy_arn):
 
 def create_deny_policy(boto_session, region, vpc_id):
 
-    text_output = ''
     # Create IAM client
     iam_client = boto_session.client('iam')
 
@@ -106,6 +116,7 @@ def create_deny_policy(boto_session, region, vpc_id):
         )
 
     except ClientError as e:
+        global text_output
         text_output = f'Unexpected error: {e}\n'
     return text_output
 
@@ -133,27 +144,29 @@ def check_for_policy(boto_session, policy_arn):
 
 def run_action(boto_session, rule, entity, params):
 
-    text_output = ''
     # getting parameters needed for all functions
     vpc_id = entity.get('id')
     region = entity.get('region')
-
-
-    # getting the vpc from ec2 recourse
-    ec2 = boto_session.resource('ec2')
-    vpc = ec2.Vpc(vpc_id)
-
-    account_id = vpc.owner_id
-    policy_arn = f"arn:aws:iam::{account_id}:policy/isolate_deny_{vpc_id}_access_policy"
-
+    global text_output
+    
     try:
-        # disabling vpc's DNS
-        vpc.modify_attribute(EnableDnsSupport={
-            'Value': False
-        },)
 
+        # getting the vpc from ec2 recourse
+        ec2_client = boto_session.client('ec2')
+        vpc = ec2_client.describe_vpcs(VpcIds=[vpc_id,],).get('Vpcs')[0]
+
+        account_id = vpc.get('OwnerId')
+        policy_arn = f"arn:aws:iam::{account_id}:policy/isolate_deny_{vpc_id}_access_policy"
+
+
+        # disabling vpc's DNS
+        ec2_client.modify_vpc_attribute(EnableDnsSupport={
+            'Value': False
+        }, VpcId=vpc_id)
+
+      
         # creating a new acl with deny all rules
-        text_output = create_acl(vpc)
+        text_output = create_acl(ec2_client, vpc_id)
 
         # checking if there was an error and if policy that limits all users in the account exist
         if text_output == '' and not check_for_policy(boto_session, policy_arn):
@@ -164,9 +177,6 @@ def run_action(boto_session, rule, entity, params):
 
         # attaching policy to all users in the account
         text_output = attach_policy_to_all_users(boto_session, policy_arn)
-
-
-
 
     except ClientError as e:
         text_output = f'Unexpected error: {e}\n'
