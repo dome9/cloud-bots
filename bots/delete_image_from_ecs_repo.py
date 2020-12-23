1d9eb598052652aac2b1985dbac7a11756bc2792
masterA11 <52865629+masterA11@users.noreply.github.com>
2020-12-23 11:00:22 +0200

    Update delete_image_from_ecs_repo.py

--- bots/delete_image_from_ecs_repo.py
+++ bots/delete_image_from_ecs_repo.py
@@ -27,7 +27,8 @@ def run_action(boto_session, rule, entity, params, report_time):
     image_name =  event['responseElements']['imageId']['imageTag']
     image_digest = event['responseElements']['imageId']['imageDigest']
 
-    ecr_client.batch_delete_image(registryId=registry_id,
+    try:
+        ecr_client.batch_delete_image(registryId=registry_id,
                                   repositoryName= repository_name,
                                   imageIds=[
                                       {
@@ -35,27 +36,5 @@ def run_action(boto_session, rule, entity, params, report_time):
                                           'imageTag' : image_name
                                       }
                                   ])
-
-def format_time(report_time):
-    # format the time from string to datetime object
-    # 2020-11-11T14:03:43.123Z
-    report_time = datetime.datetime.strptime(report_time, '%Y-%m-%dT%H:%M:%S.%fZ')
-    return report_time
-
-def find_event(boto_session, report_time):
-    # create CloudTrail client
-    client = boto_session.client('cloudtrail')
-
-    # converting the time from utc to Israel time zone
-    report_time = report_time.replace(tzinfo=tzlocal())
-    # taking 20 minutes before the alert popped in logic
-    start_time = report_time + datetime.timedelta(minutes=-20)
-
-
-
-    # get the event details
-    response = client.lookup_events(LookupAttributes=[{'AttributeKey' : 'EventName', 'AttributeValue' : EVENT_NAME}],
-                                    StartTime=start_time,
-                                    EndTime=report_time)
-
-    return response['Events'][0]['CloudTrailEvent']  if len(response['Events']) == 1 else None
+    except ClientError as e:
+        return f"Unexpected error: {e} \n"
