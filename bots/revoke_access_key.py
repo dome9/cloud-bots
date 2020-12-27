b6d8f3fceb77b0253add889fdff9851e4eb6c3c1
masterA11 <52865629+masterA11@users.noreply.github.com>
2020-12-27 18:44:54 +0200

    Update revoke_access_key.py

--- bots/revoke_access_key.py
+++ bots/revoke_access_key.py
@@ -15,15 +15,15 @@ import bots_utils
 EVENT_NAME = 'CreateAccessKey'
 
 def run_action(boto_session, rule, entity, params):
-    # create iam resource
-    iam_resource = boto3.resource('iam')
+    # create iam client
+    iam_client = boto3.client('iam')
 
     # look for the event in cloudtrail
     event = bots_utils.cloudtrail_event_lookup(boto_session, entity, EVENT_NAME)
 
     # in case that the cloudtrail_event_lookup failed to find events
     if event is None:
-        return "Error, returned 0 events"
+        return "Error when looking for the CreateAccessKey event, 0 events returned"
 
     # take the access key details from the event
     access_key_id = [i for i in event['Resources'] if i['ResourceType'] == 'AWS::IAM::AccessKey'][0]['ResourceName']
@@ -32,11 +32,11 @@ def run_action(boto_session, rule, entity, params):
     username = event['Username']
 
     try:
-        # create access key resource
-        access_key = iam_resource.AccessKey(username, access_key_id)
-
-        # revoke the access key
-        access_key.deactivate()
+        iam_client.update_access_key(
+            UserName=username,
+            AccessKeyId=access_key_id,
+            Status='Inactive'
+        )
 
         # create the text output
         return f'user: {username} access key with id : {access_key_id} was revoked'
