5096fdd228b22ef9681e43d205069b2df1467d56
kerenshm <kerensh@checkpoint.com>
2021-12-30 15:16:39 +0200

    modified cloudbot

--- bots/s3_enable_encryption.py
+++ bots/s3_enable_encryption.py
@@ -1,39 +1,53 @@
 '''
 ## s3_enable_encryption
-What it does: Turns on AES-256 encryption on the target bucket  
-Usage: AUTO: s3_enable_encryption  
-Limitations: none  
+What it does: Turns on encryption on the target bucket.
+Usage: AUTO: s3_enable_encryption <encryption_type> <kms-key-arn> (<kms-key-arn> should be provided only if <encryption_type> is KMS)
+Note: <encryption_type> can be one of the following:
+1. sse-s3 (for s3-managed keys)
+2. kms (for customer managed keys) - for kms you MUST provide the <kms-key-arn>.
+EXAMPLES:
+s3_enable_encryption s3
+s3_enable_encryption kms arn:aws:kms:us-east-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab
 '''
 
-import boto3
-from botocore.exceptions import ClientError
 
-## Turn on S3 AES-256 encryption
-def run_action(boto_session,rule,entity,params):
+def run_action(boto_session, rule, entity, params):
+    if not 1 <= len(params) <= 2:
+        raise ValueError(
+            f"Error: Wrong use of s3_enable_encryption. Usage: s3_enable_encryption <encryption_type> <kms-key-arn> (<kms-key-arn> should be provided only if <encryption_type> is KMS) \n")
+    encryption_type = params[0].lower()
+    if len(params) == 1 and encryption_type == 'kms':
+        raise ValueError(
+            f"Error: Wrong use of s3_enable_encryption. If <encryption_type> is KMS, you must provide the key's arn. \n")
+
+    if encryption_type == 'kms':
+        kms_arn = params[1]
+        encryption_config = {'SSEAlgorithm': 'aws:kms', 'KMSMasterKeyID': kms_arn}
+    elif encryption_type == 's3':
+        encryption_config = {'SSEAlgorithm': 'AES256'}
+
     bucket_name = entity['id']
     s3_client = boto_session.client('s3')
 
-    try:
-        result = s3_client.put_bucket_encryption(
-            Bucket=bucket_name,
-            ServerSideEncryptionConfiguration={
-                'Rules': [
-                    {
-                        'ApplyServerSideEncryptionByDefault': {
-                            'SSEAlgorithm': 'AES256'
-                        }
-                    },
-                ]
-            }
-        )
+    print(f'{__file__} - Trying to enable encryption... \n')
+    result = s3_client.put_bucket_encryption(
+        Bucket=bucket_name,
+        ServerSideEncryptionConfiguration={
+            'Rules': [
+                {
+                    'ApplyServerSideEncryptionByDefault': encryption_config
+                },
+            ]
+        }
+    )
 
-        responseCode = result['ResponseMetadata']['HTTPStatusCode']
-        if responseCode >= 400:
-            text_output = "Unexpected error: %s \n" % str(result)
-        else:
-            text_output = "Bucket encryption enabled: %s \n" % bucket_name
+    responseCode = result['ResponseMetadata']['HTTPStatusCode']
+    if responseCode >= 400:
+        raise ValueError("Unexpected error: %s \n" % str(result))
+    else:
+        print(f'{__file__} - Done. \n')
+        text_output = f"Bucket encryption has successfully enabled. Bukcet name: {bucket_name}, encryption type: {encryption_type}. \n"
+        if encryption_type == 'kms':
+            text_output += f"Bucket encrypted with aws key arn: {params[1]} \n"
 
-    except ClientError as e:
-        text_output = "Unexpected error: %s \n" % e
-
-    return text_output 
+    return text_output
