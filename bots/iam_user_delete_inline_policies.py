2f19f7c97779d02b72862b2bc361b159913597b7
Keren <kerensh@checkpoint.com>
2022-04-18 12:45:10 +0300

    fixed bug

--- bots/iam_user_delete_inline_policies.py
+++ bots/iam_user_delete_inline_policies.py
@@ -1,15 +1,16 @@
 """
 ## iam_user_delete_inline_policies
-What it does: deleted all iam user inline policies and attach new maneged policies if pass as an argument
-Usage: AUTH: iam_user_delete_inline_policies (option)maneged_policies=policy1_arn,policy2_arn
+What it does: deleted all iam user inline policies and attach new managed policies if passed as an argument
+Usage: iam_user_delete_inline_policies <managed_policies_arn> (<managed_policies_arn> is optional. For more than one policy, use a comma as a separator).
+- iam_user_delete_inline_policies (only deletes)
+- iam_user_delete_inline_policies policy1_arn,policy2_arn
 """
 
-import boto3
 from botocore.exceptions import ClientError
 
 
 def attach_policies(params, iam_resource, user_name) -> str:
-    policies_arn = params['maneged_policies'].replace(" ", '').split(',')
+    policies_arn = params[0].replace(" ", '').split(',')
     iam_user = iam_resource.User('name', user_name)
 
     text_output = ''
@@ -18,7 +19,7 @@ def attach_policies(params, iam_resource, user_name) -> str:
             iam_user.attach_policy(PolicyArn=policy_arn)
             text_output += f"Attached policy {policy_arn}"
         except ClientError as e:
-            text_output += f"Error {e} while attaching policy {policy_arn}\n"
+            raise ValueError(f"Error {e} while attaching policy {policy_arn}\n")
 
     return text_output
 
@@ -36,7 +37,7 @@ def run_action(session, rule, entity, params):
             user_policy.delete()
             text_output += f"Deleted policy: {policy_name}\n"
         except ClientError as e:
-            text_output += f"Error {e} while deleting policy {policy_name}\n"
+            raise ValueError(f"Error {e} while deleting policy {policy_name}\n")
 
     if len(params) > 0:
         text_output += attach_policies(params, iam_resource, user_name)
