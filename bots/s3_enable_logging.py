cec64e29449be60624770552df0f5c9c7c6db8a4
omerar <omerar@checkpoint.com>
2023-10-15 14:37:59 +0300

    changes

--- bots/s3_enable_logging.py
+++ bots/s3_enable_logging.py
@@ -6,25 +6,27 @@ Limitations: none
 '''
 
 import boto3
+import json
 from botocore.exceptions import ClientError
 
-## Turn on S3 bucket logging 
-def run_action(boto_session,rule,entity,params):
+
+## Turn on S3 bucket logging
+def run_action(boto_session, rule, entity, params):
     bucket_name = entity['id']
     accountNumber = entity['accountNumber']
-    region = entity['region'].replace("_","-")
+    region = entity['region'].replace("_", "-")
     text_output = ""
-    
+
     s3_client = boto_session.client('s3')
     s3_resource = boto_session.resource('s3')
     bucket_logging = s3_resource.BucketLogging(bucket_name)
 
     target_bucket_name = accountNumber + "s3accesslogs" + region
 
-    #The target bucket needs to be in the same region as the remediation bucket or it'll throw a CrossLocationLoggingProhibitted error.
+    # The target bucket needs to be in the same region as the remediation bucket or it'll throw a CrossLocationLoggingProhibitted error.
     try:
-        #Check if the bucket exists. If not, create one
-        s3_client.head_bucket(Bucket=target_bucket_name) #Check if the bucket exists. If not, create one
+        # Check if the bucket exists. If not, create one
+        s3_client.head_bucket(Bucket=target_bucket_name)  # Check if the bucket exists. If not, create one
 
     except ClientError:
         # The bucket does not exist or you have no access. Create it    
@@ -32,31 +34,27 @@ def run_action(boto_session,rule,entity,params):
             if region == "us-east-1":
                 result = s3_client.create_bucket(
                     Bucket=target_bucket_name,
-                    ACL='log-delivery-write'
-                    )
+                )
             elif region == "eu-west-1":
                 region = "EU"
                 result = s3_client.create_bucket(
                     Bucket=target_bucket_name,
-                    CreateBucketConfiguration={'LocationConstraint': region},
-                    ACL='log-delivery-write'
-                    )
+                    CreateBucketConfiguration={'LocationConstraint': region}
+                )
             else:
                 result = s3_client.create_bucket(
                     Bucket=target_bucket_name,
-                    CreateBucketConfiguration={'LocationConstraint': region},
-                    ACL='log-delivery-write'
-                    )
-            
+                    CreateBucketConfiguration={'LocationConstraint': region}
+                )
+
             responseCode = result['ResponseMetadata']['HTTPStatusCode']
             if responseCode >= 400:
                 text_output = "Unexpected error: %s \n" % str(result)
             else:
                 text_output = "Logging bucket created %s \n" % target_bucket_name
-       
+
         except ClientError as e:
             text_output = "Unexpected error: %s \n" % e
-
 
     try:
         result = bucket_logging.put(
@@ -70,11 +68,57 @@ def run_action(boto_session,rule,entity,params):
 
         responseCode = result['ResponseMetadata']['HTTPStatusCode']
         if responseCode >= 400:
-            text_output = text_output + "Unexpected error: %s \n" % str(result)
+            text_output = text_output + "Unexpected error enable logging: %s \n" % str(result)
+            return;
+
+        loggingPolicy = {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "logging.s3.amazonaws.com"
+                    },
+                    "Action": "s3:PutObject",
+                    "Resource": "arn:aws:s3:::" + target_bucket_name + "/*",
+                    "Condition": {
+                        "ArnLike": {
+                            "aws:SourceArn": "arn:aws:s3:::" + bucket_name}
+                    }
+                }
+
+        existing_policy = s3_resource.Bucket(target_bucket_name).Policy()
+
+        if existing_policy.meta.data is None:
+
+            policy = {
+                "Version": "2012-10-17",
+                "Statement": [loggingPolicy]}
+
+            resultUpdatePolicy = s3_resource.Bucket(target_bucket_name).Policy().put(
+                Policy=json.dumps(policy))
+
+            responseCodeUpdatePolicy = resultUpdatePolicy['ResponseMetadata']['HTTPStatusCode']
+            if responseCodeUpdatePolicy >= 400:
+                text_output = text_output + "Unexpected error Update Policy: %s \n" % str(result)
+                raise Exception(text_output);
+
         else:
-            text_output = text_output + "Bucket logging enabled from bucket: %s to bucket: %s \n" % (bucket_name,target_bucket_name)
+            existing_policy_json = json.loads(existing_policy.policy)
+
+            existing_policy_json['Statement'].append(
+                loggingPolicy
+            )
+
+            resultUpdatePolicy = s3_resource.Bucket(target_bucket_name).Policy().put(
+                Policy=json.dumps(existing_policy_json))
+
+            responseCodeUpdatePolicy = resultUpdatePolicy['ResponseMetadata']['HTTPStatusCode']
+            if responseCodeUpdatePolicy >= 400:
+                text_output = text_output + "Unexpected error Update Policy: %s \n" % str(result)
+                raise Exception(text_output);
+
+            text_output = text_output + "Bucket logging enabled from bucket: %s to bucket: %s \n" % (
+                bucket_name, target_bucket_name)
 
     except ClientError as e:
         text_output = text_output + "Unexpected error: %s \n" % e
 
-    return text_output 
+    return text_output
